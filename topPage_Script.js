2fc31d4997ccee324b4c87e1702c1815434d13f0
HSP3Widget-Official <167661524+HSP3Widget-Official@users.noreply.github.com>
2024-04-21 17:32:12 +0900

    Update topPage_Script.js

--- topPage_Script.js
+++ topPage_Script.js
@@ -1,3 +1,6 @@
 function dl_v14_jp() {
     window.location.href = "https://hsp3widget-official.github.io/Setup_14.exe";
 }
+function dl_v14_portable_jp() {
+    window.location.href = "https://hsp3widget-official.github.io/HSP3Widget_14_Protable.zip";
+}
